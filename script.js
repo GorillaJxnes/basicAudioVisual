2b401b49fd858cd5f26a9d0a6666d8898c077238
Gorilla Jones <kgorski90lol@gmail.com>
2022-10-27 02:18:15 -0500

    Initial code structure/linking files together

---
+++ script.js
@@ -0,0 +1,9 @@
+const button1 = document.getElementById("button1");
+let audio1 = new Audio();
+audio1.src = '...'
+
+button1.addEventListener("click", function () {
+
+
+  console.log("click");
+});
