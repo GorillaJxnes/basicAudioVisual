e409302a6ac5206d27d7d96955bac85932d40719
Gorilla Jones <kgorski90lol@gmail.com>
2022-10-27 03:15:38 -0500

    Realized a critical error and fixed my over complication of code

--- script.js
+++ script.js
@@ -4,22 +4,16 @@ const button3 = document.getElementById("button3");
 let audio1 = new Audio();
 audio1.src = "creep.wav";
 
-// The super simple play button.
+// Play Button.
 button1.addEventListener("click", function () {
   audio1.play();
 });
-// Pause button to stop current audio being played.
-button2.addEventListener("click", function (button1) {
-  if (button1 === true) {
-    console.log("Connected to pause button");
-  } else {
-    return console.log("button 1 = true button 2 fired");
-  }
+// Pause button
+button2.addEventListener("click", function () {
+  audio1.pause();
 });
-// Stop button taking in the conditions of the play and pause buttons.
-// need to find the correct way to code out the stop button. You were on the right track so lets keep moving forward with it.
-button3.addEventListener("click", function (button1, button2) {
-  if (button1 === true && button2 === false) {
-    console.log("button1 true, button 2 false");
-  }
+// Stop button
+button3.addEventListener("click", function () {
+  audio1.pause();
+  audio1.currentTime = 0;
 });
