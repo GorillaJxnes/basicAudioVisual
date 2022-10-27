f346eed91f50d95a8d4463796d8896c8277fbaa0
Gorilla Jones <kgorski90lol@gmail.com>
2022-10-27 03:03:25 -0500

    Syncing buttons with correct conditions for play/pause/stop

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
 let audio1 = new Audio();
-audio1.src = '...'
+audio1.src = "creep.wav";
 
 button1.addEventListener("click", function () {
+  audio1.play();
+});
 
-
-  console.log("click");
+button2.addEventListener("click", function (button1) {
+  if (button1 === true) {
+    console.log("Connected to pause button");
+  } else {
+    return console.log("button 1 = true button 2 fired");
+  }
+});
+button3.addEventListener("click", function (button1, button2) {
+  if (button1 === true && button2 === false) {
+    console.log("button1 true, button 2 false");
+  }
 });
